3932ad2f668a870c8d38619c84006ba68f42e446
CHANDRABABU <2100269@cognizant.com>
2024-04-17 16:09:15 +0530

    Day 3 Javascript practice

---
+++ day3.js
@@ -0,0 +1,27 @@
+const num1=Math.random()
+if(num1>0.7){
+    alert("The number is greater tan 0.7");
+}
+
+const arr=[1,2,3,4,5,6,7,8,9];
+
+for (let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+let j=0;
+while(j<arr.length){
+    console.log(arr[j]);
+    j++;
+}
+
+let k=0;
+for (let k=arr.length-1;k>=0;k--){
+    console.log(arr[k]);
+}
+
+const num2=Math.random();
+
+if((num1>0.7 && num2>0.7) || (num1<=0.2 || num2<=0.2)){
+    alert("Both the numbers are greater than 0.7 or one of the number is not greater than 0.2");
+}
---
+++ demo.js
@@ -0,0 +1,10 @@
+let a="Hi";
+function greet(arg){
+    a=arg;
+    var d="Good Morning";
+    console.log(a);
+}
+
+greet("Bye");
+console.log(d);
+a=[];
